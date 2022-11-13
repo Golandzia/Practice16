7095aa68f647413d0cded920449e8645f29c833f
Daniil Golandzia <solovevdaniil01@gmail.com>
2022-11-13 13:36:39 +0300

    final commit

--- app/src/main/java/com/example/practice16/MainActivity.kt
+++ app/src/main/java/com/example/practice16/MainActivity.kt
@@ -9,39 +9,24 @@ import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val sharedP = this.getSharedPreferences("CHANGE_THEME", MODE_PRIVATE)
-
         val theme = sharedP.getInt("my_theme", R.style.MyTheme_Dark)
-
         setTheme(theme)
-
         setContentView(R.layout.activity_main)
     }
 
     fun changeTheme(view: View){
         val shared = getSharedPreferences("CHANGE_THEME", MODE_PRIVATE)
         val theme = shared.getInt("my_theme", R.style.MyTheme_Dark)
-        if (theme==R.style.MyTheme_Dark)
-        {
-
+        if (theme==R.style.MyTheme_Dark) {
             shared.edit().clear().apply()
-
             shared.edit().putInt("my_theme", R.style.MyTheme_Light).apply()
-
             recreate()
-
         }
-        else
-        {
-
+        else {
             shared.edit().clear().apply()
-
             shared.edit().putInt("my_theme", R.style.MyTheme_Dark).apply()
-
             recreate()
-
         }
     }
-
 }
